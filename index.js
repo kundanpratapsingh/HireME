ecc889da3c3642ed826769471fd02999ea4f6980
kundanpratapsingh <singhkundan682@gmail.com>
2020-05-02 22:20:16 +0530

    added-server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("KUNDAN");
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`HireMe RUNNING ON PORT ${PORT}.`));
+
+app.get("/", (req, res) => {
+  res.send("Hello Developer");
+});
